5b6619a7e5af1155b8812ca358a47b8872377ff2
Robert James <rjames128@gmail.com>
2025-08-09 16:41:14 -0700

    Basic setup for the project is complete

--- StashManagement.AppHost/AppHost.cs
+++
@@ -1,5 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.StashManagement_API>("stashmanagement-api");
-
-builder.Build().Run();
---
+++ StashManagement.AppHost/Program.cs
@@ -0,0 +1,8 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.StashManagement_API>("stashmanagement-api");
+
+builder.AddNpmApp("stash-management", "../stash-management")
+    .WithHttpEndpoint(targetPort: 53237);
+
+builder.Build().Run();
